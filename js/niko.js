56555d194bddb00434c98b117ddfa46921586e43
hipi <yuyehack@gmail.com>
2021-03-31 17:34:52 +0800

    Update niko.js

--- js/niko.js
+++ js/niko.js
@@ -21,23 +21,51 @@ class Niko {
     return res;
   }
 
-  static getElements(selector) {
-    return document.querySelectorAll(selector);
-  }
   static makeArray(nodeList) {
     const _this = this;
     nodeList.forEach((elem, index) => {
       _this[index] = elem;
     });
   }
+
   constructor(selector) {
     const _this = this;
-    _this.ele = Niko.getElements(selector);
-    Niko.makeArray.call(_this, _this.ele);
+    if (!selector) {
+      _this.eles = [];
+    } else {
+      if (selector.nodeType === 1) {
+        // dom 节点
+        const fragment = document.createDocumentFragment();
+        fragment.appendChild(selector);
+        _this.eles = fragment.childNodes;
+      } else {
+        _this.eles = document.querySelectorAll(selector);
+      }
+    }
+    _this.length = _this.eles.length;
+    Niko.makeArray.call(_this, _this.eles);
+  }
+  eq(idx) {
+    if (idx >= 0 && idx <= this.eles.length) {
+      return new Niko(this.eles[idx]);
+    }
+    return new Niko();
+  }
+
+  hasClass(className) {
+    let result = false;
+    for (let item of this.eles) {
+      const classArr = Niko.filter(item.className.split(" "), (cls) => !!cls);
+      if (classArr.indexOf(className) > -1) {
+        result = true;
+        break;
+      }
+    }
+    return result;
   }
 
   addClass(className) {
-    for (let item of this.ele) {
+    for (let item of this.eles) {
       const classArr = Niko.filter(item.className.split(" "), (cls) => !!cls);
       classArr.push(className);
       item.className = classArr.join(" ");
@@ -46,7 +74,7 @@ class Niko {
   }
 
   removeClass(className) {
-    for (let item of this.ele) {
+    for (let item of this.eles) {
       const classArr = item.className.split(" ");
       item.className = Niko.filter(classArr, (cls) => cls !== className).join(
         " "
@@ -55,47 +83,63 @@ class Niko {
     return this;
   }
 
-  eq(idx) {
-    if (idx >= 0 && idx <= this.ele.length) {
-      return this.ele[idx];
-    }
-    return new Error("No this element");
-  }
-
-  style(styleName, styStr) {
-    for (let item of this.ele) {
+  css(styleName, styStr) {
+    for (let item of this.eles) {
       item.style[styleName] = styStr;
     }
     return this;
   }
 
   val(value) {
-    if (this.ele.length === 0) {
-      return;
+    if (this.eles.length === 0) {
+      return this;
     }
     const inset = typeof value !== "undefined";
     if (inset) {
-      for (let item of this.ele) {
+      for (let item of this.eles) {
         item.value = value;
       }
       return this;
     } else {
-      return this.ele[0].value;
+      return this.eles[0].value;
     }
   }
 
   html(dom) {
-    if (this.ele.length === 0) {
+    if (this.eles.length === 0) {
       return;
     }
     const inset = typeof dom !== "undefined";
     if (inset) {
-      for (let item of this.ele) {
+      for (let item of this.eles) {
         item.innerHTML = dom;
       }
       return this;
     } else {
-      return this.ele[0].innerHTML;
+      return this.eles[0].innerHTML;
+    }
+  }
+
+  each(fn) {
+    const _this = this;
+    for (let i = 0, len = _this.eles.length; i < len; ++i) {
+      const ele = _this.eles[i];
+      fn.call(ele, ele, i);
+    }
+  }
+
+  on(event, fn) {
+    const _this = this;
+    for (let i = 0, len = _this.eles.length; i < len; ++i) {
+      const ele = _this.eles[i];
+      ele.addEventListener(event, fn, false);
+    }
+  }
+  off(event, fn) {
+    const _this = this;
+    for (let i = 0, len = _this.eles.length; i < len; ++i) {
+      const ele = _this.eles[i];
+      ele.removeEventListener(event, fn, false);
     }
   }
 }
